f21fda090196b5574ae8c89c1708000767c87eef
Felipemacedo315 <felipemacedo315@gmail.com>
2022-02-24 12:31:17 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,32 +1,107 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 import Offer from "./componentes/offer";
 import Text from "./componentes/text";
 import Inputs from "./componentes/inputs";
+import MyContext from "./componentes/context";
+
 function App() {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [classFirstName, setClassFirstName] = useState("dataValid");
+  const [classLastName, setClassLastName] = useState("dataValid");
+  const [classEmail, setClassEmail] = useState("dataValid");
+  const [classPassword, setClassPassword] = useState("dataValid");
+  const [erroMsg, setErroMsg] = useState({
+    firstNameErro: "",
+    lastNameErro: "",
+    emailErro: "",
+    passwordErro: "",
+  });
+
   return (
     <Fragment>
-      <div className="App">
-        <div id="text">
-          <Text />
+      <MyContext.Provider value={{ data, setData }}>
+        <div className="App">
+          <div id="text">
+            <Text />
+          </div>
+          <div id="formulario">
+            <Offer />
+            <form>
+              <Inputs
+                type="text"
+                placeholder={"First Name"}
+                className={classFirstName}
+                textError={erroMsg.firstNameErro}
+              />
+              <Inputs
+                type="text"
+                placeholder={"Last Name"}
+                className={classLastName}
+                textError={erroMsg.lastNameErro}
+              />
+              <Inputs
+                type="email"
+                placeholder={"Email Address"}
+                className={classEmail}
+                textError={erroMsg.emailErro}
+              />
+              <Inputs
+                type="password"
+                placeholder={"Password"}
+                className={classPassword}
+                textError={erroMsg.passwordErro}
+              />
+
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  console.log(data);
+                  if (data.firstName.length === 0) {
+                    setClassFirstName("dataInvalid");
+                    erroMsg.firstNameErro = "First Name cannot be empty";
+                  } else {
+                    setClassFirstName("dataValid");
+                    erroMsg.firstNameErro = "";
+                  }
+                  if (data.lastName.length === 0) {
+                    setClassLastName("dataInvalid");
+                    erroMsg.lastNameErro = "Last Name cannot be empty";
+                  } else {
+                    setClassLastName("dataValid");
+                    erroMsg.lastNameErro = "";
+                  }
+                  if (data.email.length === 0 || data.email.indexOf('@')===-1 || data.email.indexOf('.')===-1)  {
+                    setClassEmail("dataInvalid");
+                    erroMsg.emailErro = "Looks like this is not an email";
+                  } else {
+                    setClassEmail("dataValid");
+                    erroMsg.emailErro = "";
+                  }
+                  if (data.password.length === 0) {
+                    setClassPassword("dataInvalid");
+                    erroMsg.passwordErro = "Password cannot be empty";
+                  } else {
+                    setClassPassword("dataValid");
+                    erroMsg.passwordErro = "";
+                  }
+                }}
+              >
+                CLAIM YOUR FREE TRIAL
+              </button>
+              <p>
+                By clicking the button, you are agreeing to our &nbsp;
+                <span>Terms and Services</span>
+              </p>
+            </form>
+          </div>
         </div>
-        <div id="formulario">
-          <Offer />
-          <form>
-            <Inputs type="text" placeholder={"First Name"} />
-            <Inputs type="text" placeholder={"Last Name"} />
-            <Inputs type="email" placeholder={"Email Address"} />
-            <Inputs type="password" placeholder={"Password"} />
-            <button onClick={(e)=>{
-               e.preventDefault()
-            }}>CLAIM YOUR FREE TRIAL</button>
-            <p>
-              By clicking the button, you are agreeing to our &nbsp;
-              <span>Terms and Services</span>
-            </p>
-          </form>
-        </div>
-      </div>
+      </MyContext.Provider>
     </Fragment>
   );
 }
--- src/componentes/inputs.jsx
+++ src/componentes/inputs.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Fragment, useContext, useEffect } from "react";
+import MyContext from "./context";
 
 function Inputs(props) {
-  return(
-    <input type={props.type} placeholder={props.placeholder}/>
-  )
+  const { data, setData } = useContext(MyContext);
+
+  return (
+    <Fragment>
+      <input
+        className={props.className}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={(d) => {
+          if (props.placeholder === "First Name") {
+            data.firstName = d.target.value;
+          }
+          if (props.placeholder === "Last Name") {
+            data.lastName = d.target.value;
+          }
+          if (props.placeholder === "Email Address") {
+            data.email = d.target.value;
+          }
+          if (props.placeholder === "Password") {
+            data.password = d.target.value;
+          }
+        }}
+      /> 
+       <p id='errorMsg'><i>{props.textError}</i></p>
+      
+    </Fragment>
+  );
 }
-export default Inputs
+export default Inputs;
